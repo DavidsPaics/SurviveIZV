6b58867d55465e261d7022c6ecb00614ab06786f
Dāvids Paičs <davids.paics@gmail.com>
2025-09-08 18:12:44 +0300

    Start some structure

--- src/SurviveIZV.cpp
+++ src/SurviveIZV.cpp
@@ -1,29 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include "Game.hpp"
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({320u, 180u}), "CMake SFML Project", sf::Style::Default);
-    window.setFramerateLimit(60);
-    sf::CircleShape shape(32.f);
-    // set the shape color to green
-    shape.setFillColor(sf::Color(100, 250, 50));
+    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
 
-    while (window.isOpen())
-    {
-
-        
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-        
-        window.clear();
-        
-        window.draw(shape);
-
-        window.display();
-    }
+    auto window = sf::RenderWindow(sf::VideoMode(desktopMode), "Survive IZV");
+    
+    Game game(window);
+    game.run();
 }
