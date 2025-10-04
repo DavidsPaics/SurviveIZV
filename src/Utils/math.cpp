4c29b0aae6703818afcf3e11669da73c86d24404
wstcube <142741262+wstcube@users.noreply.github.com>
2025-10-04 23:28:16 +0300

    create a graph from map

--- src/Utils/math.cpp
+++ src/Utils/math.cpp
@@ -1,12 +1,55 @@
 #include "Utils/math.hpp"
 #include <SFML/Graphics.hpp>
 #include <string>   
+#include <cmath>
 #include "Utils/logging.hpp"
+#include "Core/World.hpp"
+
+
+inline int coordsToMapIndex(int x, int y){
+    return globals::currentWorld->getMapSize().x*y+x;
+}
+inline sf::Vector2f mapIndexToCoords(int mapIndex){
+    int x = globals::currentWorld->getMapSize().x;
+    int y = globals::currentWorld->getMapSize().y;
+    return {static_cast<float>(x),static_cast<float>(y)};
+}
+
+
 
 //! PATHFINDING ASSUMES THE COORDINATE IS IN THE CENTER OF THE TILE
 
 inline void generateGraphFromMap(){
-    
+    int width =globals::currentWorld->getMapSize().x,height = globals::currentWorld->getMapSize().y;
+    mapGraph.resize(width*height);
+    auto isCollidable = [](int index){
+        return(globals::currentWorld->getMapTileInfo()[globals::currentWorld->getMap()[index]].flags&1);
+    };
+    auto isEmpty = [](int index){
+        return(globals::currentWorld->getMap()[index]==0);
+    };
+    int index=0;
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            if(!isEmpty(index) && !isCollidable(index)){
+                index++;
+                continue;
+            }
+            if(j!=width-1 && !isEmpty(index+1) && !isCollidable(index+1)){
+                mapGraph[index].push_back({index+1,1});
+                mapGraph[index+1].push_back({index,1});
+            }
+            if(i!=height-1 && !isEmpty(index+width) && !isCollidable(index+width)){
+                mapGraph[index].push_back({index+width,1});
+                mapGraph[index+width].push_back({index,1});
+            }
+            if(j!=width-1 && i != height-1 && !isEmpty(index+width+1) && !isCollidable(index+index+1)){
+            mapGraph[index].push_back(std::pair<int,double>(index+width+1,std::sqrt(2)));
+            mapGraph[index+width+1].push_back(std::pair<int,double>(index+width+1,std::sqrt(2)));
+            }
+            index++;
+        }
+    }
 }
 
 inline void dijkstra(int s, std::vector<int> & distance, std::vector<int> & path) {
