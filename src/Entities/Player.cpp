9abbaf41ec02c989558b9e27a34cc3e1cbefd784
Dāvids Paičs <davids.paics@gmail.com>
2025-10-06 10:39:19 +0300

    Terēze gribēja bultiņas

--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -12,10 +12,10 @@ void Player::update(float deltaTime)
 {
     // --- Input Acceleration ---
     Entity::movementDirection = {0.f, 0.f};
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) Entity::movementDirection.y -= 1.f;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) Entity::movementDirection.y += 1.f;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) Entity::movementDirection.x -= 1.f;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) Entity::movementDirection.x += 1.f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) Entity::movementDirection.y -= 1.f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) Entity::movementDirection.y += 1.f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) Entity::movementDirection.x -= 1.f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) Entity::movementDirection.x += 1.f;
 
     Entity::update(deltaTime);
 }
